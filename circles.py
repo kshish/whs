e7b6887bda4bb464be6cc26df4329407e2d24e7e
kshish <2049er@gmail.com>
2017-10-26 15:44:57 +0000

    Update circles.py

--- circles.py
+++ circles.py
@@ -1,11 +1,23 @@
 import turtle
 
 running = True
-radius=100
-pencolor=1
-angle=49
+radius=20
+angle=181
+x=0
+y=0
+x=x-(radius/2)
+y=y-(radius/2)
+pencolorR=1
+pencolorG=1
+pencolorB=1
+turtle.ht()
+turtle.pu()
+turtle.goto(-100, -100)
+turtle.pd()
+
 turtle.tracer(0, 0)
-turtle.pencolor(pencolor)
+turtle.colormode(255)
+turtle.pencolor(pencolorR, pencolorG, pencolorB)
 
 while running == True:
     turtle.circle(radius)
@@ -14,7 +26,14 @@ while running == True:
     turtle.fd(radius/2)
     turtle.pd()
     turtle.update()
-    pencolor=pencolor+1
-    if pencolor==255:
-        pencolor=1
-        
+    pencolorR=pencolorR+3
+    if pencolorR>=255:
+        pencolorR=1
+    pencolorG=pencolorG+2
+    if pencolorG>=255:
+        pencolorG=1
+    pencolorB=pencolorB+1
+    if pencolorB>=255:
+        pencolorB=1
+    turtle.pencolor(pencolorR, pencolorG, pencolorB)
+   
